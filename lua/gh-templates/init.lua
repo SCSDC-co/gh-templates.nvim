2af9d31ac187496afa6e1d994b5bdc33ae23ee2c
Sckab <giulianodeamicis2009@gmail.com>
2026-08-30 15:18:15 +0200

    fix: fix correct gitignore.get_template call

--- lua/gh-templates/init.lua
+++ lua/gh-templates/init.lua
@@ -3,7 +3,9 @@ local gitignore = require("gh-templates.gitignore")
 local M = {}
 
 M.gitignore = function()
-    gitignore.get_template()
+    gitignore.get_template(function(templates)
+        vim.print(templates)
+    end)
 end
 
 M.setup = function() end
