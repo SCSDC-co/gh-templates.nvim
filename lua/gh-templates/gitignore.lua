b098474ef76342fc194cd0415dabc69f9c4b6583
Sckab <giulianodeamicis2009@gmail.com>
2026-08-30 15:16:06 +0200

    fix: now the function for fetching the gitignores templates is async-safe

--- lua/gh-templates/gitignore.lua
+++ lua/gh-templates/gitignore.lua
@@ -4,23 +4,24 @@ local notify = require("gh-templates.notify")
 
 local M = {}
 
-M.get_template = function()
+---@param callback fun(templates: string[])
+M.get_template = function(callback)
     vim.net.request("https://api.github.com/gitignore/templates", {}, function(err, res)
         if err then
-            notify.error("An error occoured when trying to fetch the gitignore templates:\n" .. err)
+            notify.error("An error occurred when trying to fetch the gitignore templates:\n" .. err)
 
+            callback({})
             return
         end
 
-        ---@type string[]
-        local templates = vim.json.decode(res.body)
-
-        vim.print(templates)
+        callback(vim.json.decode(res.body))
     end)
 end
 
 M.get_gitignores = function()
-    M.get_template()
+    M.get_template(function(templates)
+        vim.print(templates)
+    end)
 end
 
 return M
